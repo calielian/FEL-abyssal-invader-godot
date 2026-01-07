f1dd7697d2005c2bd9be63f9d041d3e3c3e9bb8c
calielian <calielian@proton.me>
2026-01-06 21:02:16 -0300

    Primeira implementação de bala não testada feita

--- godot-port/resources/scripts/bala_base.gd
+++ godot-port/resources/scripts/bala_base.gd
@@ -4,3 +4,4 @@ extends Resource
 @export var dano := 1
 @export var perfuracao := 1
 @export var velocidade := 300
+@export var sprite: Texture2D = preload("res://images/bullets/bullet-default.png")
